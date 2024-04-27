b4761bd14cb7aef7e5672855eb5e65667621a1cf
nicjluz <108543896+nicjluz@users.noreply.github.com>
2024-04-26 22:52:09 -0400

    Updated app.py with new functionality

--- web-app/app.py
+++ web-app/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -9,18 +9,18 @@ client = MongoClient(os.environ['MONGODB_URI'], tls=True, tlsCertificateKeyFile=
 db = client[os.environ['DB_NAME']]
 collection = db[os.environ['DB_COLLECTION_NAME']]
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        city = request.form['city']
-        weather_data = collection.find_one({'city': city})
-        if weather_data:
-            return render_template('index.html', weather_data=weather_data)
-        else:
-            return render_template('index.html', error='Weather data not found for the selected city.')
+    return render_template('index.html')
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('location')
+    weather_data = collection.find_one({'city': city})
+    if weather_data:
+        return jsonify(weather_data)
     else:
-        cities = collection.distinct('city')
-        return render_template('index.html', cities=cities)
+        return jsonify(error='Weather data not found for the selected city.')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
