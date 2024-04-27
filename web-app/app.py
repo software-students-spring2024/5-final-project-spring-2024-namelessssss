e30c5ea14bf955c8ec74c620b5d01938baed28ab
nicjluz <108543896+nicjluz@users.noreply.github.com>
2024-04-26 23:11:23 -0400

    updated so it fits with html and css I have

--- web-app/app.py
+++ web-app/app.py
@@ -9,18 +9,28 @@ client = MongoClient(os.environ['MONGODB_URI'], tls=True, tlsCertificateKeyFile=
 db = client[os.environ['DB_NAME']]
 collection = db[os.environ['DB_COLLECTION_NAME']]
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = collection.find_one({'city': city})
+        if weather_data:
+            return render_template('index.html', weather_data=weather_data)
+        else:
+            return render_template('index.html', error='Weather data not found for the selected city.')
+    else:
+        cities = collection.distinct('city')
+        # Pass an empty dictionary as weather_data if it's a GET request
+        return render_template('index.html', cities=cities, weather_data={})
 
-@app.route('/weather', methods=['GET'])
-def get_weather():
-    city = request.args.get('location')
-    weather_data = collection.find_one({'city': city})
+@app.route('/weather')
+def weather():
+    location = request.args.get('location')
+    weather_data = collection.find_one({'city': location})
     if weather_data:
         return jsonify(weather_data)
     else:
-        return jsonify(error='Weather data not found for the selected city.')
+        return jsonify(error='Weather data not found for the selected location.'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
